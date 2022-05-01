1da1b10743cd4cdde3cb33f5a85028dc792aaf79
Zhen <s9621924@gmail.com>
2022-05-01 13:33:33 +0800

    v1.01

---
+++ group_29/_footer.php
@@ -0,0 +1,50 @@
+<!-- 只用來引入 -->
+<div class="basic-blo-area gray-bg pt-90 pb-50">
+			<div class="container">
+				<div class="area-title text-center">
+					<h2>專業服務</h2>
+					<p>In this place, we provide the profession service for all of our customer.</p>
+				</div>
+				<div class="con_flex gray-bg">
+                    <div class="it_flex">
+                        <a href="login.html">
+                            <img src="image/icon/icon1.svg">
+                            登陸/註冊
+                        </a>
+                    </div>
+                    <div class="it_flex">
+                        <a href="Bulletin_Board.html">
+                            <img src="image/icon/icon2_1.svg">
+                            優惠折扣
+                        </a>
+                    </div>
+                    <div class="it_flex">
+                        <a href="personal information.html">
+                            <img src="image/icon/icon3.svg">
+                            個人資料
+                        </a>
+                    </div>
+                    <!--<div class="it_flex">
+                        <img src="image/icon/icon4_2.svg">
+                        
+                    </div>-->
+                    <div class="it_flex2">
+                        <div class="it_flex3">
+                            <a href="shopping.html">
+                                <img src="image/icon/icon5_1.svg">
+                                購物車
+                            </a>
+                        </div>
+                        <div class="it_flex3">
+                            <a href="question.html">
+                                <img src="image/icon/icon5_2.svg">
+                            常見問題
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+		</div>
+			</div>
+		</div>
---
+++ group_29/_header.php
@@ -0,0 +1,89 @@
+<!-- 只用來引入 -->
+<header id="sticky-header">
+			<div class="header-area">
+				<div class="container sm-100">
+					<div class="row">
+						<div class="col-md-2 col-sm-2">
+							<div class="logo text-upper">
+								<h4><a href="index.php">costco</a></h4>
+							</div>
+						</div>
+						<div class="col-lg-5 col-md-3 col-sm-3 ">
+							<div class="search logo ">
+										<input type="text" placeholder="輸入商品名稱" name="search">
+										<a href="Search-result.html">
+											<button type="submit"><i class="fa fa-search"></i></button>
+										</a>
+										
+							</div>
+						</div>
+						<div class="col-lg-5 col-md-7 col-sm-7">
+							<div class="menu-area hidden-xs">
+								<nav>
+									<ul class="basic-menu clearfix">
+	
+										<li><a href="login.html">登入/註冊</a></li>
+	
+										<li>
+											<a>會員中心</a>
+											<ul>
+												<li><a href="memberlist.html">管理員</a></li>
+												<li><a href="personal information.html">個人資料</a></li>
+												<li><a href="Order_summary.html">訂單查詢</a></li>
+											</ul>
+										</li>
+	
+										<li>
+											<a href="question.html">常見問題</a>
+										</li>
+	
+										<li>
+											<a href="shopping.html">購物車</a>
+										</li>
+										<!-- END BLOG -->
+										<li>
+											<a>聯絡我們</a>
+											<ul>
+												<li><a href="cooperate.html">廠商合作</a></li>
+												<li><a href="report.html">問題回報</a></li>
+											</ul>
+										</li>
+									</ul>
+								</nav>
+							</div>
+							<!-- basic-mobile-menu -->
+							<div class="basic-mobile-menu visible-xs">
+								<nav id="mobile-nav">
+									<ul>
+										<li><a href="login.html">登入/註冊</a></li>
+	
+										<li>
+											<a>會員中心</a>
+											<ul>
+												<li><a href="personal information.html">個人資料</a></li>
+												<li><a href="Order_summary.html">訂單查詢</a></li>
+											</ul>
+										</li>
+	
+										<li>
+											<a href="question.html">常見問題</a>
+										</li>
+	
+										<li>
+											<a href="shopping.html">購物車</a>
+										</li>
+										<!-- END BLOG -->
+										<li><a>聯絡我們</a>
+											<ul>
+												<li><a href="cooperate.html">廠商合作</a></li>
+												<li><a href="report.html">問題回報</a></li>
+											</ul>
+										</li>
+									</ul>
+								</nav>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
