3ea9a2ef50ea29377f8c7f9633201334ec657018
allen <allenhke@gmail.com>
2022-05-07 12:45:44 +0800

    1.0

--- group_29/js/addbutton.js
+++ group_29/js/addbutton.js
@@ -1,10 +1,9 @@
-<script>
-        $(function () {
-            $("#btn_append").on("click", function () {
-                $("#div_upload").append('<div><button type="button" class="btn btn-danger btn-xs" data-placement="right" title="移除"><i class="fa fa-times" aria-hidden="true"></i></button><input type="file" name="doc_upload[]" style="display: inline-block"></div>')
-            })
-            $("#div_upload").on("click", "button.btn-danger", function () {
-                $(this).closest("div").remove();
-            })
-        });
-    </script>
+$(function () {
+        $("#btn_append").on("click", function () {
+            $("#div_upload").append('<div><button type="button" class="btn btn-danger btn-xs" data-placement="right" title="移除"><i class="fa fa-times" aria-hidden="true"></i></button><input type="file" name="doc_upload[]" style="display: inline-block"></div>')
+        })
+        $("#div_upload").on("click", "button.btn-danger", function () {
+            $(this).closest("div").remove();
+        })
+    });
+    
