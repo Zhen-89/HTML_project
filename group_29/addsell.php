9069b8526db11977e1690e123eef8792466165b5
allen <allenhke@gmail.com>
2022-05-07 12:55:00 +0800

    v1.05

--- group_29/addsell.php
+++ group_29/addsell.php
@@ -58,7 +58,7 @@
     <div class="basic-contact-form ptb-70" style="padding-top:0px;">
         <div class="container">
              <!-- left start -->
-                <?php include("_adminleftmenu.php") ;?>
+                <?php include("_adminleftmenu.php") ?>
             <!-- left end -->
             <div class="right-Content" id="rightContent">
                 <div class="clearfix" id="Contentsell">
