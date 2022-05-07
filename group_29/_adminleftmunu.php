89a9dcfc3d9bd44a5bda2d650f87fbcc8ecf85f4
allen <allenhke@gmail.com>
2022-05-07 12:52:06 +0800

    v1.0

---
+++ group_29/_adminleftmunu.php
@@ -0,0 +1,53 @@
+<div id="leftMenu">
+                <ul id="leftMenu_ul">
+                    <div id="member_center" class="clearfix">
+                        <button class="member_btn">管理中心</button>
+                    </div>
+
+                    <li id="userInfo" class="menu-nav">
+                        <strong id="strong_userInfo">會員</strong>
+                        <ul id="userInfo">
+                            <li>
+                                <a href="memberlist.php">會員總覽</a>
+                            </li>
+
+                        </ul>
+                    </li>
+                    <li id="li_buy" class="menu-nav">
+                        <strong id="strong_buy">商品</strong>
+                        <ul id="buy">
+                            <li>
+                                <a href="selllist.php">商品列表</a>
+                            </li>
+                            <li>
+                                <a href="addsell.php">新增商品</a>
+                            </li>
+                            <li>
+                                <a href=""></a>
+                            </li>
+
+                        </ul>
+                    </li>
+                    <li id="li_Order" class="menu-nav">
+                        <strong id="strong_Order">訂單</strong>
+                        <ul id="Order">
+                            <li>
+                                <a href="">退貨處理</a>
+                            </li>
+
+
+                        </ul>
+                    </li>
+                    <li id="li_anno" class="menu-nav">
+                        <strong id="strong_anno">資訊</strong>
+                        <ul id="anno">
+                            <li>
+                                <a href="annolist.php">公告列表</a>
+                            </li>
+                            <li class="navS">
+                                <a href="addanno.php">新增公告</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
