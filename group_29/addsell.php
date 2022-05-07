0812a74967f5bf8d9ce56c69f661a971bee871fb
allen <allenhke@gmail.com>
2022-05-07 12:47:21 +0800

    v1.05

--- group_29/addsell.php
+++ group_29/addsell.php
@@ -22,16 +22,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
     
     <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-    <script>
-        $(function () {
-            $("#btn_append").on("click", function () {
-                $("#div_upload").append('<div><button type="button" class="btn btn-danger btn-xs" data-placement="right"title="移除"><i class="fa fa-times" aria-hidden="true"></i></button><input type="file" name="doc_upload[]" style="display: inline-block"></div>')
-            })
-            $("#div_upload").on("click", "button.btn-danger", function () {
-                $(this).closest("div").remove();
-            })
-        });
-    </script>
+    <script src="js/addbutton.js"></script>
 </head>
 
 <body>
