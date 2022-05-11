5eb4bf03628a5ea2511fbe885f95ef0f84d01de1
allen <allenhke@gmail.com>
2022-05-11 20:12:48 +0800

    v2.0

--- group_29/memberlist.php
+++ group_29/memberlist.php
@@ -1,6 +1,6 @@
 <?php
 
-$link = mysqli_connect("localhost", "root", "root123456", "group_member") // 建立MySQL的資料庫連結
+$link = mysqli_connect("localhost", "root", "root123456", "group_29") // 建立MySQL的資料庫連結
     or die("無法開啟MySQL資料庫連結!<br>");
 
 // 送出編碼的MySQL指令
