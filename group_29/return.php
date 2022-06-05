b5755e7413daf46cd08cf360ee7a4533813bbfd6
zzxx55888 <73578381+zzxx55888@users.noreply.github.com>
2022-06-05 19:58:13 +0800

    v3.6
     剛剛有人在搞鬼

--- group_29/return.php
+++ group_29/return.php
@@ -8,16 +8,25 @@
     or die("無法開啟MySQL資料庫連結!<br>");
 
     // $sql = "SELECT * FROM member where mem_no='$no' ";// 指定SQL查詢字串
-    $sql = "update order_form set state = "."N'退貨申請中'"." , state_code ="."1"."  ";
+    $result = mysqli_query($link, "SELECT * FROM order_form WHERE state_code = '"."1"."' and order_No=".$o_no."");
+    if ($num = mysqli_num_rows($result))	
+    {
+        echo "<script> {window.alert('已經申請了');history.go(-1)} </script>";//返回上頁
+    }
+    else
+    {
+        echo "<script> {window.alert('申請成功');history.go(-1)} </script>";//返回上頁
+    }
+
+    $sql = "update order_form set state = "."N'退貨申請中'"." , state_code ="."1"." WHERE order_No=".$o_no." ";
 
     mysqli_query($link, 'SET CHARACTER SET utf8');
     mysqli_query($link,"SET collation_connection = 'utf8_unicode_ci'");
 
     //echo $sql;
     mysqli_query($link, $sql);
-
-    echo "<script> {window.alert('申請成功');history.go(-1)} </script>";//返回上頁
-
+    
+    
     mysqli_close($link); // 關閉資料庫連結
         
 
